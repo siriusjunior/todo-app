e59730a9c24f0fc64c0467ce6352a18ec6077486
yasuyuki <yasuyuki@R5WXKWFNH0noMacBook-Pro.local>
2023-03-26 12:42:09 +0900

    コンポーネント分割

---
+++ resources/ts/queries/TaskQuery.ts
@@ -0,0 +1,9 @@
+import * as api from "../api/TaskAPI"
+import {useQuery} from "react-query"
+
+const useTasks =() => {
+    return useQuery('tasks',() => api.getTasks())
+}
+export {
+    useTasks
+}
