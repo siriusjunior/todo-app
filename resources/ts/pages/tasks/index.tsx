50c9a60fe9ed73b0b9055ed769360f5bcfc9cbee
yasuyuki <yasuyuki@R5WXKWFNH0noMacBook-Pro.local>
2023-03-26 11:32:07 +0900

    axiosによるToDo一覧取得表示

--- resources/ts/pages/tasks/index.tsx
+++ resources/ts/pages/tasks/index.tsx
@@ -1,16 +1,43 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import axios from "axios"
+
+type Task = {
+    id: number
+    title: string
+    is_done: boolean
+    created_at: Date
+    updated_at: Date
+}
 
 const TaskPage: React.VFC = () => {
+
+    const [tasks, setTasks] = useState<Task[]>([])
+    const getTasks = async() => {
+        const { data } = await axios.get<Task[]>('api/tasks')
+        setTasks(data)
+    }
+    useEffect(() => {
+        getTasks()
+    })
     return(
         <>
             <form className="input-form">
                 <div className="inner">
-                    <input type="text" className="input" placeholder="TODOを入力してください。" value="" />
+                    <input type="text" className="input" placeholder="TODOを入力してください。" defaultValue="" />
                     <button className="btn is-primary">追加</button>
                 </div>
             </form>
             <div className="inner">
                 <ul className="task-list">
+                    { tasks.map(task => (
+                        <li key={task.id}>
+                            <label className="checkbox-label">
+                                <input type="checkbox" className="checkbox-input" />
+                            </label>
+                            <div><span>{task.title}</span></div>
+                            <button className="btn is-delete">削除</button>
+                        </li>
+                    ))}
                     <li>
                         <label className="checkbox-label">
                             <input type="checkbox" className="checkbox-input" />
@@ -23,7 +50,7 @@ const TaskPage: React.VFC = () => {
                             <input type="checkbox" className="checkbox-input" />
                         </label>
                         <form>
-                            <input type="text" className="input" value="編集中のTODO"/>
+                            <input type="text" className="input" defaultValue="編集中のTODO"/>
                             </form>
                         <button className="btn">更新</button>
                     </li>
